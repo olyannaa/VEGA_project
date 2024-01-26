0a189cba7dd591b525259ef31fe06aa3a5a1ad4d
Михаил Кочергин <114687659+finedandelion@users.noreply.github.com>
2024-01-26 21:49:04 +0500

    OpenApi swagger auth doc

--- vega/Controllers/AuthController.cs
+++ vega/Controllers/AuthController.cs
@@ -26,16 +26,6 @@ namespace vega.Controllers
         /// Authorizes user in system.
         /// </summary>
         /// <returns>Returns JWT</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     GET /api/Auth
-        ///     {
-        ///        "userLogin": string,
-        ///        "password": string,
-        ///     }
-        ///
-        /// </remarks>
         /// <response code="200">Returns JWT Token</response>
         /// <response code="400">If user is not registered in system or password is wrong</response>
         /// <response code="500">If Database does not store users role</response>
